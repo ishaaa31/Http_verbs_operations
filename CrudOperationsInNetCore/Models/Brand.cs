42b7e15e84fb2592362b49e3e5cb31c7f50828fe
Your Name <your.email@example.com>
2024-03-09 16:32:22 +0530

    first commit

---
+++ CrudOperationsInNetCore/Models/Brand.cs
@@ -0,0 +1,13 @@
+﻿namespace CrudOperationsInNetCore.Models
+{
+    public class Brand
+    {
+        public int ID { get; set; }
+
+        public string Name { get; set; }
+
+        public string Category { get; set; }
+
+        public int IsActive { get; set; }
+    }
+}
